8e274c52c964a76c3ceae91454bb1b1022545d41
reynaldo0 <reynaldooyusellino@gmail.com>
2024-11-11 11:01:30 +0700

    fix

--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const programs = [
-  { icon: "/icon/program/school.png", alt: "Graduation Cap", label: "Education" },
-  { icon: "/icon/program/Academy.png", alt: "Presentation", label: "Workshop" },
-  { icon: "/icon/program/Competition.png", alt: "Trophy", label: "Awards" },
-  { icon: "/icon/program/Internship.png", alt: "Computer", label: "Online Course" },
+  { icon: "/icon/program/school.png", alt: "Graduation Cap", label: "Scholarship" },
+  { icon: "/icon/program/Academy.png", alt: "Presentation", label: "Academy" },
+  { icon: "/icon/program/Competition.png", alt: "Trophy", label: "Competition" },
+  { icon: "/icon/program/Internship.png", alt: "Computer", label: "Internship" },
   { icon: "/icon/program/Volunteer.png", alt: "Community", label: "Volunteer" },
-  { icon: "/icon/program/Community.png", alt: "Handshake", label: "Collaboration" },
-  { icon: "/icon/program/Career.png", alt: "Business Growth", label: "Entrepreneurship" },
+  { icon: "/icon/program/Community.png", alt: "Handshake", label: "Community" },
+  { icon: "/icon/program/Career.png", alt: "Business Growth", label: "Career" },
 ];
 
 const Program = () => {
@@ -22,7 +22,7 @@ const Program = () => {
               <img src={program.icon} alt={program.alt} className="w-32 h-32  duration-300 group-hover:scale-110" />
               
               {/* Hidden text, shown on hover */}
-              <p className="text-3xl font-medium text-gray-700 opacity-0 group-hover:opacity-100 absolute -top-20 left-0 right-0 transition-all duration-300 transform scale-90 group-hover:scale-100 group-hover:translate-y-0">
+              <p className="text-3xl font-bold text-gray-700 opacity-0 group-hover:opacity-100 absolute -top-20 text-center transition-all duration-300 transform scale-90 group-hover:scale-100 group-hover:translate-y-0">
                 {program.label}
               </p>
             </div>
